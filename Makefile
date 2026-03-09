473d9fe622ed26a70d474f6d7e98207a1e295e5e
Ali Abdallah <ali.abdallah@suse.com>
2026-03-09 17:30:35 +0100

    Implement decode/encode base64 using openssl

--- Makefile
+++ Makefile
@@ -8,7 +8,7 @@ BINDIR=		${PREFIX}/bin
 MANDIR=		${PREFIX}/share/man/man
 LIBDIR=		${PREFIX}/lib
 
-SRCS= main.c mbox.c imap.c
+SRCS= main.c mbox.c imap.c base64.c
 
 CFLAGS+=       -I${.CURDIR} -Wall
 LDFLAGS+=      -lssl -lcrypto
---
+++ base64.c
@@ -0,0 +1,85 @@
+/*
+ * Copyright (c) 2026, Ali Abdallah <ali.abdallah@suse.com>
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the <organization> nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+#include <openssl/bio.h>
+#include <openssl/evp.h>
+#include <openssl/buffer.h>
+#include <stdint.h>
+
+char *b64_encode(const char *input, int *b64_len) {
+    char *buff;
+    BIO *bmem, *b64;
+    BUF_MEM *bptr;
+
+    b64 = BIO_new(BIO_f_base64());
+    BIO_set_close(b64, BIO_CLOSE);
+    BIO_set_flags(b64, BIO_FLAGS_BASE64_NO_NL);
+
+    bmem = BIO_new(BIO_s_mem());
+    bmem = BIO_push(b64, bmem);
+
+    BIO_write(b64, input, strlen(input));
+    BIO_flush(b64);
+
+    BIO_get_mem_ptr(b64, &bptr);
+
+    buff = (char *)malloc(bptr->length);
+    memcpy(buff, bptr->data, bptr->length);
+    buff[bptr->length] = 0;
+
+    *b64_len = bptr->length;
+
+    BIO_free_all(bmem);
+
+    return buff;
+}
+
+char *b64_decode(const char *input, int b64_len) {
+    char *buff;
+    BIO *bmem, *b64;
+
+    buff = (char *)malloc(b64_len);
+    memset(buff, 0, b64_len);
+
+    b64 = BIO_new(BIO_f_base64());
+    BIO_set_flags(b64, BIO_FLAGS_BASE64_NO_NL);
+    bmem = BIO_new(BIO_s_mem());
+    bmem = BIO_push(b64, bmem);
+
+    BIO_write(bmem, input, b64_len);
+    BIO_flush(bmem);
+    BIO_read(b64, (void*)buff, b64_len);
+
+    BIO_free_all(b64);
+
+    return buff;
+}
+
